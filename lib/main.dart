57ba3f9a02bbdf16b616eeb424730fb0e9b2a5ad
JuanWehitt <juan.cruz.wehitt@gmail.com>
2022-12-31 03:10:50 -0300

    Agregado de dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:prototipo_flutter_lab4/pages/track_page.dart';
 import 'package:prototipo_flutter_lab4/pages/pages.dart';
+import 'package:prototipo_flutter_lab4/providers/albums_provider.dart';
 import 'package:prototipo_flutter_lab4/themes/default_theme.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
