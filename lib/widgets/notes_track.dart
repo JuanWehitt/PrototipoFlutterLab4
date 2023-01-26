63876dd3a82e40d3cc7bc2a783aa093ba9ef5df6
JuanWehitt <juan.cruz.wehitt@gmail.com>
2023-01-26 06:57:03 -0300

    Modificaciones en Notes Track

--- lib/widgets/notes_track.dart
+++ lib/widgets/notes_track.dart
@@ -50,35 +50,12 @@ class NotesTrack extends StatelessWidget {
                         borderRadius: BorderRadius.all(Radius.circular(10.0))),
                   ),
                   onChanged: ((value) {
-                    Notes.addNote(track.id, value);
+                    value == ""
+                        ? Notes.quitNote(track.id)
+                        : Notes.addNote(track.id, value);
                   }),
                 ),
               ),
-              ClipRRect(
-                  borderRadius: BorderRadius.circular(4),
-                  child: Stack(children: <Widget>[
-                    Positioned.fill(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: <Color>[
-                              Color(0xFF0D47A1),
-                              Color(0xFF1976D2),
-                              Color(0xFF42A5F5),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    TextButton(
-                        style: TextButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          padding: const EdgeInsets.all(16.0),
-                          textStyle: const TextStyle(fontSize: 20),
-                        ),
-                        onPressed: () {},
-                        child: Text("Guardar")),
-                  ]))
             ],
           ),
         ),
